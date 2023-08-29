01482c43fb854bdf0c78e4de75fb561e0f865b54
sr.bala <sr.bala.kkl@gmail.com>
2023-08-29 16:24:24 +0530

    new:Zone.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, NgZone} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RunningCodeOutsideOfAngular';
+
+  constructor(private ngZone: NgZone) {
+  }
+
+  /**
+   * Zone.js is a signaling mechanism that Angular uses to detect
+   * when an application state might have changed.
+   *
+   * It captures asynchronous operations like setTimeout, network requests, and event listeners.
+   * Angular schedules change detection based on signals from Zone.js.
+   *
+   * */
+
+  ngOnInit() {
+/**    This method is used to remove the zone pollution in angular
+
+    note 1: The preceding snippet instructs Angular to call setInterval
+    outside the Angular Zone and skip running change detection
+
+    note 2: Third-party libraries commonly trigger unnecessary change
+    detection cycles because they weren't authored with Zone.js in mind.
+    Avoid these extra cycles by calling library APIs outside the Angular zone:
+
+    */
+
+    this.ngZone.runOutsideAngular(() => {
+      setTimeout(() => {
+        // update component data
+        // but don't trigger change detection.
+
+        console.log("I'mRunning Outside of angular.")
+      });
+    });
+  }
 }
